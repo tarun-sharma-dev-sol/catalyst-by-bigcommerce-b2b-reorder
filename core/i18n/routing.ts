2230ce0e621f18c787f27a76c538e7de50e22284
tarun-sharma-dev-sol <188996527+tarun-sharma-dev-sol@users.noreply.github.com>
2025-09-10 05:07:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ core/i18n/routing.ts
@@ -0,0 +1,27 @@
+import { createNavigation } from 'next-intl/navigation';
+import { defineRouting } from 'next-intl/routing';
+
+import { defaultLocale, locales } from './locales';
+
+enum LocalePrefixes {
+  ALWAYS = 'always',
+  // Don't use NEVER as there is a issue that causes cache problems and returns the wrong messages.
+  // More info: https://github.com/amannn/next-intl/issues/786
+  // NEVER = 'never',
+  ASNEEDED = 'as-needed', // removes prefix on default locale
+}
+
+const localePrefix = LocalePrefixes.ASNEEDED;
+
+export const routing = defineRouting({
+  locales,
+  defaultLocale,
+  localePrefix,
+});
+
+// Lightweight wrappers around Next.js' navigation APIs
+// that will consider the routing configuration
+// Redirect will append locale prefix even when in default locale
+// More info: https://github.com/amannn/next-intl/issues/1335
+export const { Link, redirect, usePathname, useRouter, permanentRedirect } =
+  createNavigation(routing);
